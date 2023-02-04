f12a591aea64126e94dcc9a478dc12b2b86a70b6
Harald Koch <harald.koch@gmail.com>
2023-02-04 13:10:10 -0500

    feat: promtheus-dnssec-exporter (#6)

---
+++ apps/prometheus-dnssec-exporter/ci/latest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+version=$(curl -sX GET "https://api.github.com/repos/haraldkoch/prometheus-dnssec-exporter/releases/latest" | jq --raw-output '. | .tag_name' 2>/dev/null)
+version="${version#*v}"
+version="${version#*release-}"
+printf "%s" "${version}"
