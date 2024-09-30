208dfaebb5329338d7e3a67d9a0adcfdd60ac8e6
Harald Koch <harald.koch@gmail.com>
2024-09-30 13:53:42 -0400

    add rsync

---
+++ apps/rsync/ci/latest.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".github/scripts"))
+
+from apksearch import get_latest_version
+
+if __name__ == "__main__":
+    version = get_latest_version("rsync")
+    print(f"{version}")
