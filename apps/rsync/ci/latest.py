bf141211cbc29878ffe9ebe841d6cf0e8c50edcd
Harald Koch <harald.koch@gmail.com>
2024-09-30 14:09:50 -0400

    switch latest.sh to latest.py (#272)
    * switch latest.sh to latest.py

---
+++ apps/rsync/ci/latest.py
@@ -0,0 +1,13 @@
+import sys
+import os
+sys.path.append(os.path.abspath(".github/scripts"))
+
+from apksearch import apksearch
+
+def get_latest(_: str) -> str | None:
+    """Get the latest version of rsync."""
+    return apksearch("rsync")
+
+if __name__ == "__main__":
+    version = get_latest("stable")
+    print(f"{version}")
