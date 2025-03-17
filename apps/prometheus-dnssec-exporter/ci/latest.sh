a3f0965a9a54a8ac1aabc17c8e2e5d0bafc6e9f5
Harald Koch <harald.koch@gmail.com>
2025-03-17 15:58:42 -0400

    convert prometheus-dnssec-exporter to new build

--- apps/prometheus-dnssec-exporter/ci/latest.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-version=$(curl -sX GET "https://api.github.com/repos/haraldkoch/prometheus-dnssec-exporter/releases/latest" | jq --raw-output '. | .tag_name' 2>/dev/null)
-version="${version#*v}"
-version="${version#*release-}"
-printf "%s" "${version}"
