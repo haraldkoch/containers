928595e6007ecaedfa77c7352330ab127ca5e018
Harald Koch <harald.koch@gmail.com>
2026-06-16 15:56:40 -0400

    feat: forgejo-runner

---
+++ apps/forgejo-runner/container_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"testing"
+
+	"github.com/haraldkoch/container-images/testhelpers"
+)
+
+func Test(t *testing.T) {
+	ctx := context.Background()
+	image := testhelpers.GetTestImage("ghcr.io/haraldkoch/forgejo-runner:rolling")
+
+	t.Run("Check cosign exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/cosign", nil)
+	})
+
+	t.Run("Check flux exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/flux", nil)
+	})
+
+	t.Run("Check flux-local exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/root/.local/bin/flux-local", nil)
+	})
+
+	t.Run("Check flate exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/flate", nil)
+	})
+
+	t.Run("Check helm exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/helm", nil)
+	})
+
+	t.Run("Check kustomize exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/local/bin/kustomize", nil)
+	})
+
+	t.Run("Check rsync exists", func(t *testing.T) {
+		testhelpers.TestFileExists(t, ctx, image, "/usr/bin/rsync", nil)
+	})
+}
