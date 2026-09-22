86730eca8070e084bc42db0fe64a07298350ceb1
Harald Koch <harald.koch@gmail.com>
2026-09-21 21:17:19 -0400

    feat: calibre-web (#712)
    * feat: calibre-web
    
    * chore: fix tests
    
    * chore: still more tests
    
    * chore: wheeee!
    
    * chore: someday I will get this right
    
    * chore: latest version

---
+++ apps/calibre-web/container_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"testing"
+
+	helpers "github.com/haraldkoch/containers/tests"
+)
+
+func Test(t *testing.T) {
+	image := helpers.GetTestImage("ghcr.io/haraldkoch/calibre-web:rolling")
+
+	t.Run("HTTP endpoint test", func(t *testing.T) {
+		helpers.RequireHTTPEndpoint(t, image, helpers.HTTPTestConfig{Port: "8083"}, nil)
+	})
+
+	t.Run("Check /opt/kepubify/kepubify exists", func(t *testing.T) {
+		helpers.RequireFileExists(t, image, "/opt/kepubify/kepubify")
+	})
+
+	t.Run("Check /opt/calibre/ebook-convert exists", func(t *testing.T) {
+		helpers.RequireFileExists(t, image, "/opt/calibre/ebook-convert")
+	})
+}
