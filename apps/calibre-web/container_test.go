e6d9da164cefd064104abb7a9ecbd5572890a924
Harald Koch <harald.koch@gmail.com>
2026-09-21 20:41:59 -0400

    chore: fix tests

--- apps/calibre-web/container_test.go
+++ apps/calibre-web/container_test.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"context"
 	"testing"
 
-	"github.com/haraldkoch/containers/testhelpers"
+	helpers "github.com/haraldkoch/containers/tests"
 )
 
 func Test(t *testing.T) {
-	ctx := context.Background()
 	image := testhelpers.GetTestImage("ghcr.io/haraldkoch/calibre-web:rolling")
 
 	t.Run("HTTP endpoint test", func(t *testing.T) {
-		testhelpers.TestHTTPEndpoint(t, ctx, image, testhelpers.HTTPTestConfig{
-			Port: "8083",
-		}, nil)
+		testhelpers.RequireHTTPEndpoint(t, image, testhelpers.HTTPTestConfig{Port: "8083"}, nil)
 	})
 
 	t.Run("Check /opt/kepubify/kepubify exists", func(t *testing.T) {
-		testhelpers.TestFileExists(t, ctx, image, "/opt/kepubify/kepubify", nil)
+		testhelpers.RequireFileExists(t, image, "/opt/kepubify/kepubify", nil)
 	})
 
 	t.Run("Check /opt/calibre/ebook-convert exists", func(t *testing.T) {
-		testhelpers.TestFileExists(t, ctx, image, "/opt/calibre/ebook-convert", nil)
+		testhelpers.RequireFileExists(t, image, "/opt/calibre/ebook-convert", nil)
 	})
 }
